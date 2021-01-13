487f5fc0ceb16ecca28ffe5c6e1a821d5e749aea
unknown <travisbostick@gmail.com>
2021-01-13 02:58:34 -0600

    created bubble plot and demographic. need to make it responsive and make the panel pretty

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,7 +3,7 @@
 var dropdown = d3.select("#selDataset");
 
 function dropdown_change(){
-    console.log(dropdown.property("value"));
+    // console.log(dropdown.property("value"));
 
 
     //// updating the graph
@@ -12,8 +12,12 @@ function dropdown_change(){
 
             ///// define currention option selected by the test subject dropdown
             var cur_val = dropdown.property("value");
+
             // defines the sample for the selected id
+            
             var cur_sample = d.samples.find(element=> element.id == cur_val);
+
+            //////////////// need to sort cur_sample.otu_ids and cur_sample.sample_values together greatest to least based on the sample_value, then eliminate 
 
             /// defining the data and layout to update
             var dataupdate ={
@@ -36,10 +40,13 @@ function dropdown_change(){
 
 
 
+
+///// as it sits now, this all happens upon startup
 //opens the json file
 d3.json("samples.json").then(function(d){
 
 
+    //// maybe make this a seperate function?
     // adds the names(aka IDs) to the drop down list
     d3.select("#selDataset")
         .selectAll('option')
@@ -48,25 +55,79 @@ d3.json("samples.json").then(function(d){
         .append("option")
         .html(x => x);
 
-        ///// define currention option selected by the test subject dropdown
-        var cur_val = dropdown.property("value");
-        // defines the sample for the selected id
-        var cur_sample = d.samples.find(element=> element.id == cur_val);
+    
+    //////////////////////////////////////////////////////////////////
+    //// getting the data for the bar graph and
+    ///// define currention option selected by the test subject dropdown
+    var cur_val = dropdown.property("value");
+    // defines the sample for the selected id
+    var cur_sample = d.samples.find(element=> element.id == cur_val);
 
-        var tracedata = [{
-            x:cur_sample.otu_ids,
-            y:cur_sample.sample_values,
-            type:"bar"
-        }];
+    ///// need to sort cur_sample.otu_ids and cur_sample.sample_values together greatest to least based on the sample_value, then eliminate 
 
-        var layout = {
-            title: `results for ${cur_val}`
-          };
 
-        Plotly.newPlot("bar", tracedata, layout);
+    var tracedata1 = [{
+        x:cur_sample.otu_ids,
+        y:cur_sample.sample_values,
+        type:"bar",
+        orientation:'h'
+    }];
+
+    var layout1 = {
+        title: `results for ${cur_val}`
+        };
+
+    Plotly.newPlot("bar", tracedata1, layout1);
+    //////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////
+    //// bubble chart
+    var tracedata2=[{
+        x:cur_sample.otu_ids,
+        y:cur_sample.sample_values,
+        // text:cur_sample.otu_labels,
+        mode:'markers',
+        marker:{
+            size:cur_sample.sample_values,
+            color:cur_sample.otu_ids
+        }
+
+    }];
+
+    var layout2 = {
+        title: `Bubble Chart for ${cur_val}`,
+        showlegend: false,
+        height: 600,
+        width: 600
+    };
+
+    Plotly.newPlot('bubble',tracedata2,layout2);
+    //////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////
+    //////// getting the demographics on the pange
+    var cur_demo=d.metadata.find(element => element.id == cur_val);
+    //////// get the panel body and add a list
+    //ugly but it works for now
+    d3.select("#sample-metadata").append("ul") 
+        .selectAll('li')
+        .data(Object.entries(cur_demo))
+        .enter()
+        .append("li")
+        .html(x => x);
+    
+
+    /////////////////////////////////////////////////////////////
+
+
+
+
 
 
 });
+
+
+
 
 
 
